31485d860bd7a540762a3c101ceb2becd9a2dde9
Thysrael <2470583476@qq.com>
2023-10-31 19:47:23 +0800

    fix(shell): ssh tmux error

--- core/init-shell.el
+++ core/init-shell.el
@@ -8,7 +8,8 @@
   (setq vterm-shell "zsh")
   :config
   (setq vterm-tramp-shells '(("docker" "/bin/bash")
-                             ("ssh" "/bin/bash"))) ; 指定在 tramp 时使用的 shell
+                             ("ssh" "'/bin/bash'"))) ; 指定在 tramp 时使用的 shell
+  ; /bin/bash 先加双引号，后加单引号可以解决 ssh tmux error 具体文档如下 https://github.com/akermu/emacs-libvterm/issues/569
   ;; (setq vterm-always-compile-module t)
   (defun +start-vterm-in-project ()
   "Start vterm in the current project if available, otherwise start a regular vterm."
