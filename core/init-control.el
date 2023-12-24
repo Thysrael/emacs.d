4b5ae708424faf24b73a1b265c80dd52339c08c5
Thysrael <2470583476@qq.com>
2023-12-24 11:11:44 +0800

    feat(control): init org-capture

---
+++ core/init-control.el
@@ -0,0 +1,31 @@
+;;; -*- lexical-binding: t -*-
+
+(use-package org
+  :bind
+  ("C-c o" . org-capture) ; org-capture
+  :config
+  (setq org-default-notes-file "~/learn/org/inbox.org")
+  (setq org-capture-templates '())
+  (add-to-list 'org-capture-templates
+               '("b" "Book Reading" entry
+                 (file+olp "" "Reading")
+                 "* TODO %^{Book Name} %^g\n%U\n%?"))
+  (add-to-list 'org-capture-templates
+               '("e" "Emacs Config" entry
+                 (file+olp "" "Emacs Config")
+                 "* TODO %^{Config or Bug} %^g\n%U\n%?"))
+  (add-to-list 'org-capture-templates
+               '("p" "Blog Post" entry
+                 (file+olp "" "Blog")
+                 "* TODO %^{Idea} %^g\n%U\n%?"))
+  (add-to-list 'org-capture-templates
+               '("m" "Misc" entry
+                 (file+olp "" "Misc")
+                 "* TODO %^{Misc} %^g\n%U\n%?"))
+  (add-to-list 'org-capture-templates
+               '("w" "Word" entry
+                 (file+olp "~/learn/org/words.org" "Inbox")
+                 "* TODO %^{Word}\n%?"))
+  (setq org-todo-keywords
+        '((sequence "TODO(t!)" "PEND(p!)" "|" "DONE(d!)" "ABORT(a@/!)")))
+  )
