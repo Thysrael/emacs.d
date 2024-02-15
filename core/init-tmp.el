f50ac3e1208dfcd0fd3395dc0b8c42041882d56a
Thysrael <2470583476@qq.com>
2024-02-15 21:48:37 +0800

    feat(write): add ox-latex

--- core/init-tmp.el
+++ core/init-tmp.el
@@ -67,6 +67,181 @@
 ;;
 ;; (setq-default eglot-workspace-configuration #'+patch/eglot-pyright-venv-workspace-config)
 
+(setq org-latex-pdf-process
+      '("xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"
+        "xelatex -interaction nonstopmode -output-directory %o %f"))
+
+(with-eval-after-load 'ox-latex
+  ;; http://orgmode.org/worg/org-faq.html#using-xelatex-for-pdf-export
+  ;; latexmk runs pdflatex/xelatex (whatever is specified) multiple times
+  ;; automatically to resolve the cross-references.
+  (setq org-latex-pdf-process '("latexmk -xelatex -quiet -shell-escape -f %f"))
+  (add-to-list 'org-latex-classes
+               '("elegantpaper"
+                 "\\documentclass[lang=cn]{elegantpaper}
+                   [NO-DEFAULT-PACKAGES]
+                   [PACKAGES]
+                   [EXTRA]"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  ;; 如果 org 设置了#+LATEX_HEADER: \usepackage{minted} 的话 则支持代码高亮和代码格式化
+  (setq org-latex-listings 'minted)
+  ;; Windows 加了这句的话 org 里 C-c C-x C-l 无法预览公式
+  ;; 为了兼容性就不加这句 然后 org 里加上 #+LATEX_HEADER: \usepackage{minted} 即可
+  ;; (add-to-list 'org-latex-packages-alist '("minted"))
+
+  ;; 解决 org 文件里面执行  org-latex-preview 生成的 .tex 格式不对的问题
+  ;; 通过 log 可以看到错误信息 ! Package minted Error: You must invoke LaTeX with the -shell-escape flag
+  )
+
+
+;; (use-package meow
+;;   :straight t
+;;   :hook (after-init . meow-global-mode)
+;;   :demand t
+;;   :custom-face
+;;   (meow-normal-indicator ((t (:inherit (font-lock-function-name-face bold :inverse-video t)))))
+;;   (meow-insert-indicator ((t (:inherit (font-lock-keyword-face bold :inverse-video t)))))
+;;   (meow-keypad-indicator ((t (:inherit (font-lock-builtin-face bold :inverse-video t)))))
+;;   (meow-beacon-indicator ((t (:inherit (font-lock-type-face bold :inverse-video t)))))
+;;   (meow-motion-indicator ((t (:inherit (font-lock-doc-face bold :inverse-video t)))))
+;;   :config
+;;   (setq-default meow-replace-state-name-list '((normal . "N")
+;;                                                (motion . "M")
+;;                                                (keypad . "K")
+;;                                                (insert . "I")
+;;                                                (beacon . "B")))
+;;
+;;   ;; [motion]
+;;   (meow-motion-overwrite-define-key
+;;    '("j" . meow-next)
+;;    '("k" . meow-prev)
+;;    '("<escape>" . ignore))
+;;
+;;   ;; [leader]
+;;   (meow-leader-define-key
+;;    ;; SPC j/k will run the original command in MOTION state.
+;;    '("j" . "H-j")
+;;    '("k" . "H-k")
+;;    ;; Use SPC (0-9) for digit arguments.
+;;    '("1" . meow-digit-argument)
+;;    '("2" . meow-digit-argument)
+;;    '("3" . meow-digit-argument)
+;;    '("4" . meow-digit-argument)
+;;    '("5" . meow-digit-argument)
+;;    '("6" . meow-digit-argument)
+;;    '("7" . meow-digit-argument)
+;;    '("8" . meow-digit-argument)
+;;    '("9" . meow-digit-argument)
+;;    '("0" . meow-digit-argument)
+;;    '("/" . meow-keypad-describe-key)
+;;    '("?" . meow-cheatsheet))
+;;
+;;   ;; [normal]
+;;   (meow-normal-define-key
+;;    '("0" . meow-expand-0)
+;;    '("9" . meow-expand-9)
+;;    '("8" . meow-expand-8)
+;;    '("7" . meow-expand-7)
+;;    '("6" . meow-expand-6)
+;;    '("5" . meow-expand-5)
+;;    '("4" . meow-expand-4)
+;;    '("3" . meow-expand-3)
+;;    '("2" . meow-expand-2)
+;;    '("1" . meow-expand-1)
+;;    '("-" . negative-argument)
+;;    '(";" . meow-reverse)
+;;    '("," . meow-inner-of-thing)
+;;    '("." . meow-bounds-of-thing)
+;;    '("[" . meow-beginning-of-thing)
+;;    '("]" . meow-end-of-thing)
+;;    '("a" . meow-append)
+;;    '("A" . meow-open-below)
+;;    '("b" . meow-back-word)
+;;    '("B" . meow-back-symbol)
+;;    '("c" . meow-change)
+;;    '("d" . meow-delete)
+;;    '("D" . meow-backward-delete)
+;;    '("e" . meow-next-word)
+;;    '("E" . meow-next-symbol)
+;;    '("f" . meow-find)
+;;    '("g" . meow-cancel-selection)
+;;    '("G" . meow-grab)
+;;    '("h" . meow-left)
+;;    '("H" . meow-left-expand)
+;;    '("i" . meow-insert)
+;;    '("I" . meow-open-above)
+;;    '("j" . meow-next)
+;;    '("J" . meow-next-expand)
+;;    '("k" . meow-prev)
+;;    '("K" . meow-prev-expand)
+;;    '("l" . meow-right)
+;;    '("L" . meow-right-expand)
+;;    '("m" . meow-join)
+;;    '("n" . meow-search)
+;;    '("o" . meow-block)
+;;    '("O" . meow-to-block)
+;;    '("p" . meow-yank)
+;;    '("q" . meow-quit)
+;;    '("Q" . meow-goto-line)
+;;    '("r" . meow-replace)
+;;    '("R" . meow-swap-grab)
+;;    '("s" . meow-kill)
+;;    '("t" . meow-till)
+;;    '("u" . meow-undo)
+;;    '("U" . meow-undo-in-selection)
+;;    '("v" . meow-visit)
+;;    '("w" . meow-mark-word)
+;;    '("W" . meow-mark-symbol)
+;;    '("x" . meow-line)
+;;    '("X" . meow-goto-line)
+;;    '("y" . meow-save)
+;;    '("Y" . meow-sync-grab)
+;;    '("z" . meow-pop-selection)
+;;    '("'" . repeat)
+;;    '("<escape>" . ignore))
+;;
+;;   (dolist
+;;       (state
+;;        '((telega-root-mode . motion)
+;;          (telega-chat-mode . normal)
+;;          (View-mode . normal)
+;;          ;; (compilation-mode . normal)
+;;          (blink-search-mode . insert)
+;;          (rcirc-mode . normal)
+;;          (comint-mode . normal)         ; IELM
+;;          (fundamental-mode . normal)
+;;          (message-mode . normal)
+;;          (emacs-lisp-mode . normal)
+;;          (eshell-mode . insert)
+;;          (shell-mode . insert)
+;;          (term-mode . insert)
+;;          (vterm-mode . insert)
+;;          (help-mode . normal)
+;;          (vundo-mode . motion)))
+;;     (add-to-list 'meow-mode-state-list state))
+;;  )
+
+;; (use-package iscroll
+;;   :straight t)
+
+;; (setq tree-sitter-load-path "~/.emacs.d/tree-sitter/")
+;;
+;; (use-package msgu
+;;   :straight (msgu :type git :host github :repo "jcs-elpa/msgu"))
+;;
+;;
+;; (use-package ts-docstr
+;;   :straight (ts-docstr :type git :host github :repo "emacs-vs/ts-docstr"
+;;                        :files (:defaults "langs/*.el"))
+;;   :init
+;;   (setq ts-docstr-key-support t))
+
 
 ;; (use-package org-tree-slide
 ;;   :straight t
