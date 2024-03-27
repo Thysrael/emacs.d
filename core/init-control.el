d8fc1d0440394bec83652c73619773299f45a42f
Thysrael <2470583476@qq.com>
2024-03-27 09:41:38 +0800

    chore(control): add tag for org report

--- core/init-control.el
+++ core/init-control.el
@@ -172,8 +172,8 @@
   (org-agenda-search-headline-for-time nil)
   ;; 提前 3 天截止日期到期告警
   (org-deadline-warning-days 3)
-  ;; clock report level 增加到 3
-  (org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3))
+  ;; clock report level 增加到 3 ，显示 tag
+  (org-agenda-clockreport-parameter-plist '(:link t :maxlevel 3 :tags t))
   ;; :custom-face
   ;; (org-schedule ((t (:family "Sarasa Mono SC"))))
   ;; (org-scheduled ((t (:family "Sarasa Mono SC"))))
