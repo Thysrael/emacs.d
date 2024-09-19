2d1ec6be3bbfe07e5c3ab3e134171b2119f1ee34
Thysrael <thysrael@163.com>
2024-09-19 17:49:50 +0800

    feat(tmp): try dogears

--- core/init-tmp.el
+++ core/init-tmp.el
@@ -20,3 +20,24 @@
 
 ;; (use-package nano-vertico
 ;;   :straight (nano-vertico :type git :host github :repo "rougier/nano-vertico"))
+
+;; 无法实时预览，所以功能不强
+;; (use-package dogears
+;;   :straight t
+;;   :hook (after-init . dogears-mode)
+;;   :config
+;;   (setq dogears-idle 1
+;;         dogears-limit 200
+;;         dogears-position-delta 20)
+;;   (setq dogears-functions '(find-file recenter-top-bottom
+;;                                       other-window switch-to-buffer
+;;                                       aw-select toggle-window-split
+;;                                       windmove-do-window-select
+;;                                       pager-page-down pager-page-up
+;;                                       tab-bar-select-tab
+;;                                       pop-to-mark-command
+;;                                       pop-global-mark
+;;                                       goto-last-change
+;;                                       xref-go-back
+;;                                       xref-find-definitions
+;;                                       xref-find-references)))
