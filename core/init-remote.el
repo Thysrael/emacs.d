cd8352ad6dc7bab49cd0fb59f44df6c99592766b
Thysrael <thysrael@163.com>
2024-10-23 17:24:23 +0800

    feat(remote): increase tramp speed

--- core/init-remote.el
+++ core/init-remote.el
@@ -5,7 +5,11 @@
   (setq tramp-default-method "ssh"
         tramp-auto-save-directory (no-littering-expand-var-file-name "tramp-autosaves/")
         tramp-backup-directory-alist backup-directory-alist
-        remote-file-name-inhibit-cache 60)
+        remote-file-name-inhibit-cache 60 ; 加速，允许 cache
+        remote-file-name-inhibit-locks t ; 加速，不会使用文件锁
+        tramp-verbose 1 ; 加速，更少的 tramp 信息
+        vc-handled-backends '(SVN Git) ; 加速，禁用一些版本控制后端
+        )
   (setq tramp-ssh-controlmaster-options
         "-o ControlMaster=auto -o ControlPath=tramp.%%C -o ControlPersist=no -t")
   ;; 只需要输入一次密码 https://www.reddit.com/r/emacs/comments/3liwm7/is_it_possible_to_configure_tramp_such_that_i/
