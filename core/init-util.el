c2d651ce8915bdca7e6f861caa8a5cabc79c9ba3
Thysrael <thysrael@163.com>
2025-03-24 09:50:14 +0800

    chore(init): remove redudant startup optimizaton

--- core/init-util.el
+++ core/init-util.el
@@ -14,8 +14,8 @@
   )
 
 ; 使用 M-x esup 就可以显示关键路径
-(use-package esup
-  :straight t)
+;; (use-package esup
+;;   :straight t)
 
 ;; 展示颜色，用于 CSS 或者 HTML，当有 256 进制颜色的时候，可以 overlay 出颜色
 (use-package rainbow-mode
--- init.el
+++ init.el
@@ -1,7 +1,4 @@
-;; 一个启动优化，通过将 file-name-handler-alist 置 nil 来避免检查
-;; magic file 机制
-(defconst my-saved-file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
+;;; -*- lexical-binding: t -*-
 
 (defvar +init-files (list
                      'init-package
@@ -39,6 +36,3 @@
 (let ((init-directory (expand-file-name "core/" user-emacs-directory)))
   (dolist (file +init-files)
     (load-file (concat init-directory (symbol-name file) ".el"))))
-
-;; 重新恢复 file-name-handler-alist
-(setq file-name-handler-alist my-saved-file-name-handler-alist)
