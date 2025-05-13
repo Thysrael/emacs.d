f9f37994955f285be1051ef8faa02e05d0efc9b0
Thysrael <thysrael@163.com>
2025-05-13 15:12:53 +0800

    chore(dired): workaround for dirvish load

--- core/init-dired.el
+++ core/init-dired.el
@@ -45,8 +45,12 @@
 
 ;; 使用 `E` 可以用外部命令打开文件
 (use-package dirvish
-  :ensure t
+  ;; :ensure t
+  :vc (:url "https://github.com/alexluigit/dirvish" :lisp-dir "extensions/")
+  :load-path "./elpa/dirvish/"
+  ;; :load-path "./elpa/dirvish"
   :init
+  (require 'dirvish)
   (dirvish-override-dired-mode)
   :custom-face
   (dirvish-hl-line ((t (:inherit hl-line))))
