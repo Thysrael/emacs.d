d92264ee2de66a5a0aba77e097281d14218359a9
Thysrael <thysrael@163.com>
2026-08-06 13:10:00 +0800

    feat(workspace): new tab name

--- core/init-workspace.el
+++ core/init-workspace.el
@@ -5,11 +5,7 @@
   :ensure nil
   :bind
   (("M-t" . tab-new)
-   ("M-w" . tab-close)
-   ("M-k" . tab-next)
-   ("M-j" . tab-previous)
-   :map tab-bar-mode-map
-   ("C-<tab>" . nil))
+   ("M-w" . tab-close))
   :custom
   (tab-bar-auto-width t)
   (tab-bar-auto-width-max nil)
@@ -23,15 +19,35 @@
   (tab-bar-tab-name-function #'thy/tab-bar-tab-name)
   :preface
   (defun thy/tab-bar-tab-name ()
-    "Return the current tab name with its window count when needed."
-    (let* ((raw-tab-name (buffer-name (window-buffer (minibuffer-selected-window))))
+    "Return the current tab name with its remote host and window count."
+    (let* ((buffer (window-buffer (minibuffer-selected-window)))
+           (raw-tab-name (buffer-name buffer))
+           (hostname
+            (with-current-buffer buffer
+              (and default-directory
+                   (file-remote-p default-directory 'host))))
+           (remote-name
+            (and hostname
+                 (not (string-equal hostname "localhost"))
+                 (concat " @" hostname)))
            (count (length (window-list-1 nil 'nomini))))
-      (if (> count 1)
-          (format "%s(%d)" raw-tab-name count)
-        raw-tab-name)))
+      (concat raw-tab-name
+              remote-name
+              (and (> count 1) (format "(%d)" count)))))
 
   (defun thy/tab-bar-tab-name-format (tab index)
     "Return TAB with one leading space before its default label."
     (let ((face (funcall tab-bar-tab-face-function tab)))
       (concat (propertize " " 'face face)
-              (tab-bar-tab-name-format-default tab index)))))
+              (tab-bar-tab-name-format-default tab index))))
+  :config
+  (custom-theme-set-faces
+   'user
+   '(tab-bar ((t (:inherit mode-line-inactive :box nil))))
+   '(tab-bar-tab
+     ((t (:inherit mode-line
+          :foreground "#f8f8f2" :background "#6272a4"
+          :weight bold :underline nil :box nil))))
+   '(tab-bar-tab-inactive
+     ((t (:inherit mode-line-inactive
+          :weight normal :underline nil :box nil))))))
